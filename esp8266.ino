ed2d361d2e899b2140f44a2005932cc7e3c10295
heeyoon5757 <105828770+heeyoon5757@users.noreply.github.com>
2024-03-18 17:57:49 +0000

    Update esp8266.ino

--- esp8266.ino
+++ esp8266.ino
@@ -1,103 +1,24 @@
-#include <ESP8266WiFi.h>
- 
-const char* ssid = "Heeyun"; // fill in here your router or wifi SSID
-const char* password = "heeyunchang"; // fill in here your router or wifi password
- #define RELAY 0 // relay connected to  GPIO0
-WiFiServer server(80);
- 
-void setup() 
-{
-  Serial.begin(115200); // must be same baudrate with the Serial Monitor
- 
-  pinMode(RELAY,OUTPUT);
-  digitalWrite(RELAY, LOW);
- 
-  // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
- 
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
- 
-  // Start the server
-  server.begin();
-  Serial.println("Server started");
- 
-  // Print the IP address
-  Serial.print("Use this URL to connect: ");
-  Serial.print(WiFi.localIP(172.20.10.6));
-  Serial.println("/");
- 
-}
- 
-void loop() 
-{
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) 
-  {
-    return;
-  }
- 
-  // Wait until the client sends some data
-  Serial.println("new client");
-  while(!client.available())
-  {
-    delay(1);
-  }
+#define MOTION_SENSOR_PIN D2
+#define LED_PIN D3
 
-  // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
- 
-  // Match the request
-  int value = LOW;
-  if (request.indexOf("/RELAY=ON") != -1)  
-  {
-    Serial.println("RELAY=ON");
-    digitalWrite(RELAY,LOW);
-    value = LOW;
-  }
-  if (request.indexOf("/RELAY=OFF") != -1)  
-  {
-    Serial.println("RELAY=OFF");
-    digitalWrite(RELAY,HIGH);
-    value = HIGH;
+bool motionDetected = false;
+
+void setup() {
+  pinMode(MOTION_SENSOR_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  motionDetected = digitalRead(MOTION_SENSOR_PIN);
+  
+  if (motionDetected) {
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("Motion Detected - LED ON");
+  } else {
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("No Motion Detected - LED OFF");
   }
   
-  // Return the response
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println(""); //  this is a must
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
-  client.println("<head><title>ESP8266 RELAY Control</title></head>");
-  client.print("Relay is now: ");
- 
-  if(value == HIGH) 
-  {
-    client.print("OFF");
-  } 
-  else 
-  {
-    client.print("ON");
-  }
-  client.println("<br><br>");
-  client.println("Turn <a href=\"/RELAY=OFF\">OFF</a> RELAY<br>");
-  client.println("Turn <a href=\"/RELAY=ON\">ON</a> RELAY<br>");
-    client.println("</html>");
- 
-  delay(1);
-  Serial.println("Client disonnected");
-  Serial.println("");
+  delay(500); // Adjust delay as needed
 }

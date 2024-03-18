5e5fa322dff5263fb75b5dbd63053b963970cb03
heeyoon5757 <105828770+heeyoon5757@users.noreply.github.com>
2024-03-18 18:16:25 +0000

    Update esp8266.ino

--- esp8266.ino
+++ esp8266.ino
@@ -1,11 +1,15 @@
+#include <DmxSimple.h>
+
 #define MOTION_SENSOR_PIN D2
-#define LED_PIN D3
+#define DMX_CHANNEL 1 // The DMX channel to control the LED
+#define MOTION_DETECTED_LEVEL HIGH // Level at which motion is detected
 
 bool motionDetected = false;
 
 void setup() {
   pinMode(MOTION_SENSOR_PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
+  DmxSimple.usePin(3); // Specify the pin used for DMX communication
+  DmxSimple.maxChannel(1); // Set the maximum DMX channel
   Serial.begin(115200);
 }
 
@@ -13,10 +17,10 @@ void loop() {
   motionDetected = digitalRead(MOTION_SENSOR_PIN);
   
   if (motionDetected) {
-    digitalWrite(LED_PIN, HIGH);
+    DmxSimple.write(DMX_CHANNEL, 255); // Turn on the LED at full brightness
     Serial.println("Motion Detected - LED ON");
   } else {
-    digitalWrite(LED_PIN, LOW);
+    DmxSimple.write(DMX_CHANNEL, 0); // Turn off the LED
     Serial.println("No Motion Detected - LED OFF");
   }
   
